c406c90b42c589282fb427c4cce6e2d1fba1d3ac
djeukeu <dmxchristian@gmail.com>
2025-02-11 17:22:26 +0000

    pretty and lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
+  bracketSpacing: true,
   bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
 };
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+import { getDefaultConfig, mergeConfig } from '@react-native/metro-config';
 
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+export default mergeConfig(getDefaultConfig(__dirname), config);
