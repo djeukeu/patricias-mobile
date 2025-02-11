46472f7a353eabc293ce02b215a9a633b87f4b89
djeukeu <dmxchristian@gmail.com>
2025-02-11 16:53:19 +0000

    env

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import config from './src/config';
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>{config.env}</Text>
     </View>
   );
 };
