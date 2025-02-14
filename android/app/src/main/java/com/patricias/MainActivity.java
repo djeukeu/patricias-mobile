cb6b11c006c07b4cf836259a26962417c8fbeb34
djeukeu <dmxchristian@gmail.com>
2025-02-14 12:32:39 +0000

    android package id

--- android/app/src/main/java/com/patricias/MainActivity.java
+++ android/app/src/main/java/com/patricias/MainActivity.java
@@ -1,4 +1,4 @@
-package com.patricias;
+package com.patriciaskinscentsecrets.shop;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/patricias/MainApplication.java
+++ android/app/src/main/java/com/patricias/MainApplication.java
@@ -1,4 +1,4 @@
-package com.patricias;
+package com.patriciaskinscentsecrets.shop;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
