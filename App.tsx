700f9668788965965b4f720cb968b99e1eb6287a
djeukeu <dmxchristian@gmail.com>
2025-02-14 13:05:46 +0000

    redux

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
 import i18nInstance from './src/i18n';
 import Navigation from './src/Navigation';
+import store from './src/store';
 
 import 'moment/locale/fr';
 
 const App = () => {
   return (
     <I18nextProvider i18n={i18nInstance}>
-      <Navigation />
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
     </I18nextProvider>
   );
 };
