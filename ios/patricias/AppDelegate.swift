56661c3b0406faec9f442afb49699ae61f319dd3
djeukeu <dmxchristian@gmail.com>
2025-08-05 21:15:05 +0100

    feat: splash screen and app logo

--- ios/patricias/AppDelegate.swift
+++ ios/patricias/AppDelegate.swift
@@ -29,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       launchOptions: launchOptions
     )
 
+    RNSplashScreen.show()
     return true
   }
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import AppNavigator from './AppNavigator';
 
 const Navigation = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
