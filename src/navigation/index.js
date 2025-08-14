9964183e5ba8facbf02818c1c65ab89d92162b5e
djeukeu <dmxchristian@gmail.com>
2025-08-14 14:09:08 +0100

    feat: language handler

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,15 +4,18 @@ import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
 import { useColorScheme } from 'react-native';
 import { adaptNavigationTheme } from 'react-native-paper';
 import SplashScreen from 'react-native-splash-screen';
 import AppNavigator from './AppNavigator';
 import { CurrencyContext } from '../context/CurrencyProvider';
+import { storeLng } from '../utils/language';
 
 const Navigation = () => {
   const theme = useColorScheme();
   const currencyCtx = useContext(CurrencyContext);
+  const { i18n } = useTranslation();
 
   const { LightTheme, DarkTheme } = adaptNavigationTheme({
     reactNavigationLight: NavigationDefaultTheme,
@@ -20,11 +23,14 @@ const Navigation = () => {
   });
 
   useEffect(() => {
-    const setup = Promise.all([currencyCtx.initCurrency()]);
+    const setup = Promise.all([
+      currencyCtx.initCurrency(),
+      storeLng(i18n.language),
+    ]);
     setup.then(() => {
       SplashScreen.hide();
     });
-  }, []);
+  }, [i18n.language]);
 
   return (
     <NavigationContainer theme={theme === 'dark' ? DarkTheme : LightTheme}>
